e66cf7efc37b62fc5a487d9c64a517bf3be045f4
liqinwen <15909309330@163.com>
2019-09-30 14:28:21 +0800

    test

---
+++ test/src/main/java/com/cy/cookie/TestCookie.java
@@ -0,0 +1,19 @@
+package com.cy.cookie;
+import java.util.Date;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//cookie原理实现
+public class TestCookie extends HttpServlet {
+
+	private static final long serialVersionUID = -8905355612882309354L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		//解决乱码
+		response.setContentType("text/html;charset=utf-8");
+		//返回页面上一次的访问时间
+		Date date = new Date();
+		System.out.println(date);
+	}
+}
+	
